c7fce6fdb273562547099adb04c954183df78161
Jacqueline <jackieo5023@gmail.com>
2020-03-21 02:49:33 -0500

    Restructure

--- api/src/__init__.py
+++ api/src/__init__.py
@@ -1,29 +1,35 @@
 from flask import Flask, request
 from flask_pymongo import PyMongo
+from flask_sqlalchemy import SQLAlchemy
 from bson.json_util import dumps
-from werkzeug.middleware.proxy_fix import ProxyFix
+
+# db = SQLAlchemy()
+
+app = Flask(__name__, instance_relative_config=False)
+app.config.from_object('config.Config')
+
+# db.init_app(app)
 
 mongo = PyMongo()
-app = Flask(__name__, instance_relative_config=False)
-app.wsgi_app = ProxyFix(app.wsgi_app)
-app.config.from_object('config.Config')
 mongo.init_app(app)
+
+@app.route("/api/sql")
+def index():
+    return "Hello, world!"
 
 @app.route("/api/mongo/test")
 def insert_test():
     # This just inserts a new entry into the collection "testdb", document "test" where the name of the entry is "test"
     # I just wanted to provide a basic example and also make sure this worked
-    print(x)
     client = mongo.cx
     x = client.testdb.test.insert_one({"name": "test"})
-    print(x)
     return dumps({"id": x.inserted_id})
 
 # def create_app():
 #     """Construct the core application."""
 #     app = Flask(__name__, instance_relative_config=False)
 #     app.config.from_object('config.Config')
-#     mongo.init_app(app)
+#     # db.init_app(app)
 
 #     with app.app_context():
 #         from . import routes
