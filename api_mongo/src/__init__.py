c83989b1700f27533453c5a5002d351716168f74
Jacqueline <jackieo5023@gmail.com>
2020-03-21 00:58:26 -0500

    Add import

--- api_mongo/src/__init__.py
+++ api_mongo/src/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
+from werkzeug.contrib.fixers import ProxyFix
 
 mongo = PyMongo()
 app = Flask(__name__, instance_relative_config=False)
--- api_sql/src/__init__.py
+++ api_sql/src/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.contrib.fixers import ProxyFix
 
 # db = SQLAlchemy()
 
