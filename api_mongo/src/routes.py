30e3929150223c0962fd5a5d346e59bb0e1a63f4
Jacqueline <jackieo5023@gmail.com>
2020-03-20 22:07:46 -0500

    Edit now json

--- api_mongo/src/routes.py
+++ api_mongo/src/routes.py
@@ -3,7 +3,7 @@ from bson.json_util import dumps
 from . import mongo
 
 # https://www.w3schools.com/python/python_mongodb_insert.asp
-@app.route("/api/test", methods=['POST'])
+@app.route("/api/mongo/test", methods=['POST'])
 def insert_test():
     # This just inserts a new entry into the collection "testdb", document "test" where the name of the entry is "test"
     # I just wanted to provide a basic example and also make sure this worked
--- api_sql/src/routes.py
+++ api_sql/src/routes.py
@@ -1,6 +1,6 @@
 from flask import current_app as app, request
 from .models import db
 
-@app.route("/api")
+@app.route("/api/sql")
 def index():
     return "Hello, world!"
