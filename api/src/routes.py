0f2d9d10436ee936d3518a7e3268408e23f54699
tianren2 <tianren2@illinois.edu>
2020-04-06 17:17:49 -0500

    connect to mongodb atlas

--- api/src/routes.py
+++ api/src/routes.py
@@ -1,6 +1,7 @@
 from flask import current_app as app, request
 from bson.json_util import dumps
 from . import mongo
+import pymongo
 
 @app.route("/api")
 def home():
@@ -11,6 +12,15 @@ def home():
 def insert_test():
     # This just inserts a new entry into the collection "testdb", document "test" where the name of the entry is "test"
     # I just wanted to provide a basic example and also make sure this worked
-    client = mongo.cx
-    x = client.testdb.test.insert_one({"name": "test"})
+
+    client = pymongo.MongoClient("mongodb://test:cs411@cluster0-shard-00-00-llf4k.mongodb.net:27017,cluster0-shard-00-01-llf4k.mongodb.net:27017,cluster0-shard-00-02-llf4k.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+    db = client.test
+
+    x = db.test.insert_one({"name": "test"})
     return dumps({"id": x.inserted_id})
+@app.route("/workouts")
+def workouts_insert():
+    client = pymongo.MongoClient("mongodb://test:cs411@cluster0-shard-00-00-llf4k.mongodb.net:27017,cluster0-shard-00-01-llf4k.mongodb.net:27017,cluster0-shard-00-02-llf4k.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+    db = client.test
+    x = db.workouts.insert_one({"type": "running"})
+    return "Workouts loaded."
