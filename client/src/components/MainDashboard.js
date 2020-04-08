fba960f4f40857ed26a046711d26d3b7ff289b80
ananyacleetus <ananyacleetus@gmail.com>
2020-04-08 02:21:37 -0400

    first two pages

---
+++ client/src/components/MainDashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class MainDashboard extends Component{
+    render(){
+        return(
+            <div>
+                <h1>This is the dashboard</h1>
+            </div>
+        );
+    }
+}
+export default MainDashboard;
---
+++ client/src/components/UserProfile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class UserProfile extends Component{
+    render(){
+        return(
+            <div>
+                <h1>User Profile Page</h1>
+            </div>
+        );
+    }
+}
+export default UserProfile;
