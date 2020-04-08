cfe8314e01f5a87ed598ab36f62bd38ef744779a
ananyacleetus <ananyacleetus@gmail.com>
2020-04-08 02:24:39 -0400

    nutrition, exercise, and settings

---
+++ client/src/components/ExercisePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class ExercisePage extends Component{
+    render(){
+        return(
+            <div>
+                <h1>This is the exercise page</h1>
+            </div>
+        );
+    }
+}
+export default ExercisePage;
---
+++ client/src/components/NutritionPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class NutritionPage extends Component{
+    render(){
+        return(
+            <div>
+                <h1>This is the nutrition page</h1>
+            </div>
+        );
+    }
+}
+export default NutritionPage;
