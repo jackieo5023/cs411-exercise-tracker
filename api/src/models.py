df04bc9f53df038447d8867856821412453634ac
tianren2 <tianren2@illinois.edu>
2020-04-09 11:07:23 -0500

    basic functionality added

--- api/src/models.py
+++ api/src/models.py
@@ -7,7 +7,10 @@ class Gender(enum.Enum):
     FEMALE = "Female"
     MALE = "Male"
     OTHER = "Other"
-
+eats = db.Table('eats',
+        db.Column('id', db.Integer, db.ForeignKey('Person.id'), primary_key=True),
+        db.Column('recipeId', db.Integer, db.ForeignKey('Recipe.recipeId'), primary_key=True)
+    )
 class Person(db.Model):
     """Model for user accounts"""
     # We will need to add more things to represent the relationships
@@ -20,7 +23,8 @@ class Person(db.Model):
     weight = db.Column(db.Integer, nullable=False)
     height = db.Column(db.Integer, nullable=False)
     age = db.Column(db.Integer, nullable=False)
-
+    eats = db.relationship('Recipe', secondary=eats, lazy='subquery',
+                           backref=db.backref('people', lazy=True))
     def __init__(self, firstName, lastName, gender, weight, height, age):
         self.firstName = firstName
         self.lastName = lastName
@@ -31,3 +35,26 @@ class Person(db.Model):
 
     def __repr__(self):
         return "<Person {}>".format(self.publicId)
+
+    # Recipe(recipeId: int, recipeName: string, protein: int,
+    # vitamin: float[an array of volume of different kinds of vitamin], calories: float)
+
+class Recipe(db.Model):
+    """Model for Recipes"""
+    # more interaction with mongodb later
+
+    __tablename__ = 'Recipe'
+    recipeId = db.Column(db.Integer, primary_key=True)
+    recipeName = db.Column(db.String(255), nullable=False)
+    protein = db.Column(db.Integer, nullable=False)
+    vitamin = db.Column(db.ARRAY(db.Float), nullable=False)
+    calories = db.Column(db.Integer,nullable = False)
+
+    def __init__(self,name,protein,vitamin,calories):
+        self.recipeName - name
+        self.protein = protein
+        self.vitamin = vitamin
+        self.calories = calories
+        
+    def __repr__(self):
+        return "<Recipe {}>".format(self.recipeName)
--- api/src/routes.py
+++ api/src/routes.py
@@ -20,12 +20,78 @@ def insert_test():
     x = db.test.insert_one({"name": "test"})
     return dumps({"id": x.inserted_id})
 
-@app.route("/workouts")
+@app.route("/api/workouts_reload", methods=['POST'])
+def workouts_reload():
+    client = pymongo.MongoClient("mongodb://test:cs411@cluster0-shard-00-00-llf4k.mongodb.net:27017,cluster0-shard-00-01-llf4k.mongodb.net:27017,cluster0-shard-00-02-llf4k.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+    body = request.get_json()
+    if not body:
+        return {"status": 400, "message": "Invalid body"}
+    reload_db = body.get("reload_db")
+    if (reload_db == True):
+        db = client.test
+        try:
+            x = db.workouts.drop()
+            if (x == False):
+                return {"status": 400, "message": "Collection drop failed"}
+            
+            x = db.workouts.insert_one({"type": "Bicycling", "METs": "8.0"})
+            x = db.workouts.insert_one({"type": "Jumping rope", "METs": "12.3"})
+            x = db.workouts.insert_one({"type": "Hatha Yoga", "METs": "2.5"})
+            x = db.workouts.insert_one({"type": "Running", "METs": "9.8"})
+            x = db.workouts.insert_one({"type": "Golf", "METs": "4.3"})
+            x = db.workouts.insert_one({"type": "Tennis", "METs": "8.0"})
+            x = db.workouts.insert_one({"type": "Basketball", "METs": "6.5"})
+            x = db.workouts.insert_one({"type": "Swimming laps", "METs": "5.8"})
+            x = db.workouts.insert_one({"type": "Hiking", "METs": "7.3"})
+            x = db.workouts.insert_one({"type": "Stationary cycling", "METs": "6.8"})
+            x = db.workouts.insert_one({"type": "Circuit training", "METs": "8.0"})
+            x = db.workouts.insert_one({"type": "Yardwork", "METs": "5.0"})
+            x = db.workouts.insert_one({"type": "Gardening", "METs": "3.8"})
+            x = db.workouts.insert_one({"type": "Walking for exercise", "METs": "4.3"})
+            x = db.workouts.insert_one({"type": "Exercise/activity-based video game", "METs": "3.8"})
+            x = db.workouts.insert_one({"type": "Video-exercise (DVD/TV) cardio", "METs": "4.0"})
+        except pymongo.errors.DuplicateKeyError as e:
+            return {"status": 409, "message": "Workouts already exists"}
+        except Exception as e:
+            return {"status": 500, "message": "Something went wrong"}
+    return {"status": 201, "message": "Workouts reloaded"}
+
+@app.route("/api/workouts_insert", methods=['POST'])
 def workouts_insert():
     client = pymongo.MongoClient("mongodb://test:cs411@cluster0-shard-00-00-llf4k.mongodb.net:27017,cluster0-shard-00-01-llf4k.mongodb.net:27017,cluster0-shard-00-02-llf4k.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+    body = request.get_json()
+    if not body:
+        return {"status": 400, "message": "Invalid body"}
+    workoutName = body.get("workoutName")
+    mets = body.get("METs")
+    if not workoutName or not mets:
+        return {"status": 400, "message": "Missing field"}
     db = client.test
-    x = db.workouts.insert_one({"type": "running"})
-    return "Workouts loaded."
+    try:
+        ins_id = db.workouts.insert_one({"type": workoutName,"Mets": mets})
+    except IntegrityError:
+        return {"status": 409, "message": "Workouts already exists"}
+    except Exception as e:
+        return {"status": 500, "message": "Something went wrong"}
+    return {"status": 201, "message": "Workouts added","id": ins_id["id"]}
+
+@app.route("/api/workouts_delete", methods=['DELETE'])
+def workouts_delete():
+    body = request.get_json()
+    if not body:
+        return {"status": 400, "message": "Invalid body"}
+    workoutName = body.get("workoutName")
+    if not workoutName:
+        return {"status": 400, "message": "Missing field"}
+    client = pymongo.MongoClient("mongodb://test:cs411@cluster0-shard-00-00-llf4k.mongodb.net:27017,cluster0-shard-00-01-llf4k.mongodb.net:27017,cluster0-shard-00-02-llf4k.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+    db = client.test
+    try:
+        x = db.workouts.delete_one({"type":workoutName})
+    except pymongo.errors.CursorNotFound as e:
+        return {"status": 409, "message": "Workouts not found"}
+    except Exception as e:
+        return {"status": 500, "message": "Something went wrong"}
+    return {"status": 201, "message": "Workouts deleted"}
 
 @app.route("/api/register", methods=['POST'])
 def register():
@@ -78,3 +144,70 @@ def login():
     if person:
         return {"status": 200, "message": "Person found", "id": person_id}
     return {"status": 404, "message": "Person not found"}
+
+@app.route("/api/find_recipe", methods=['GET'])
+def find_recipe():
+    body = request.get_json()
+    if not body:
+        return {"status": 400, "message": "Invalid body"}
+    recipeName = body.get("recipeName")
+    if not recipeName:
+        return {"status": 400, "message": "Missing field"}
+    result = db.session.execute(
+        "SELECT protein,vitamin,calories FROM Recipe WHERE name =:recipeName",
+        {"name": recipeName},
+    )
+    Facts = result.fetchone()
+    result.close()
+
+    if Facts:
+        return {"status": 200, "message": "Recipe found"}
+    return {"status": 404, "message": "Recipe not found"}
+
+@app.route("/api/insert_recipe", methods=['POST'])
+def insert_recipe():
+    body = request.get_json()
+    if not body:
+        return {"status": 400, "message": "Invalid body"}
+    name = body.get("recipeName")
+    protein = body.get("protein")
+    vitamin = body.get("vitamin")
+    calories = body.get("calories")
+
+    if not name or not protein or not vitamin or not calories:
+        return {"status": 400, "message": "Missing field"}
+    try:
+        result = db.session.execute(
+            "INSERT INTO Recipe (name, protein, vitamin, calories) VALUES (:name, :protein, :vitamin, :calories)",
+            {"name": name, "protein": protein, "vitamin": vitamin, "calories": calories},
+        )
+        db.session.commit()
+    except IntegrityError:
+        return {"status": 409, "message": "Recipe already exists"}
+    except Exception as e:
+        return {"status": 500, "message": "Something went wrong"}
+
+    ins_id = db.session.execute("SELECT LAST_INSERT_ID() as id").fetchone()
+    return {"status": 201, "message": "Recipe created", "id": ins_id["id"]}
+
+@app.route("/api/delete_recipe", methods=['DELETE'])
+def delete_recipe():
+    body = request.get_json()
+    if not body:
+        return {"status": 400, "message": "Invalid body"}
+    name = body.get("recipeName")
+
+    if not name:
+        return {"status": 400, "message": "Invalid body"}
+    
+    try:
+        result = db.session.execute(
+            "DELETE FROM Recipe WHERE name = :name",
+            {"name": name},
+        )
+        db.session.commit()
+    except Exception as e:
+        return {"status": 500, "message": "Something went wrong"}
+    if not result:
+        return {"status": 404, "message": "Recipe not found"}
+    return {"status": 201, "message": "Recipe deleted"}
